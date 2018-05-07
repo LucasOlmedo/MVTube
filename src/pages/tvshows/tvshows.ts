ff57dcd9265e281f9b50cef41881c889cb35f62e
lucasolmedo <lucas.silva@e-htl.com.br>
2018-05-07 18:02:33 -0300

    fix unused import

--- src/pages/tvshows/tvshows.ts
+++ src/pages/tvshows/tvshows.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, ViewController, Content } from 'ionic-angular';
 import { PopcornApiProvider } from '../../providers/popcorn-api/popcorn-api';
 import { TvshowDetailPage } from '../tvshow-detail/tvshow-detail';
-import { HomePage } from '../home/home';
 import { SettingsProvider } from '../../providers/settings/settings';
 import { FILTER } from '../../constants/api.constants';
 import { FilterModalPage } from '../filter-modal/filter-modal';
@@ -39,7 +38,7 @@ export class TvshowsPage {
     this.timestamp = Math.floor(Date.now() / 1000);
     this.tvshows = navParams.get('tvshows');
     this.page = 1;
-    settings.getActiveTheme()
+    this.settings.getActiveTheme()
       .subscribe(theme => this.selectedTheme = theme);
   }
 
